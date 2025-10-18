7d416e4a7168739992f28832589ae14a31527409
GrigorievaLisa <bob@gmail.com>
2025-10-19 00:21:44 +0300

    build: Изменение типа переменных

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // you may type whatever you want
 int main() {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
 
     cout << "A + B = " << a + b << '\n'
